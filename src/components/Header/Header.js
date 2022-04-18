f937e2bdca43bc90db8100f694f7ea1f59c783a9
Adam Lenger <adam.lenger@guidevision.cz>
2022-04-18 20:33:35 +0200

    Exercise 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,9 +29,18 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 72px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  gap: 48px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
